6716049a4bc6558512e19d34951738b144fdd1ae
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-19 22:42:51 +0300

    Generating user Routes and Controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+    #creating our show method 
+    def show
+        #Find a user in the database using the user id from the session hash 
+        user = find_by(id: session[:user_id])
+        #returning the JSON as object 
+        render json: user
+
+    end
+
 end
