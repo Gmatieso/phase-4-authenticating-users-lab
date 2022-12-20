272c163fbd25eed23bee9253296e0a159616387e
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-20 16:40:57 +0300

    Final Code Refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-    #creating our show method 
     def show
-        #Find a user in the database using the user id from the session hash 
-        user = User.find_by(id: session[:user_id])
-        #returning the JSON as object 
+      user = User.find_by(id: session[:user_id])
+      if user
         render json: user
-
+      else
+        render json: { error: "Not authorized" }, status: :unauthorized
+      end
     end
-
-end
+  end
+  
