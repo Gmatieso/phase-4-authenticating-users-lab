5bee5cd6788a3d159783701d9b07e6c129d3ffa9
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-20 05:56:40 +0300

    find user in the database using the user id from the session hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     #creating our show method 
     def show
         #Find a user in the database using the user id from the session hash 
-        user = find_by(id: session[:user_id])
+        user = User.find_by(id: session[:user_id])
         #returning the JSON as object 
         render json: user
 
